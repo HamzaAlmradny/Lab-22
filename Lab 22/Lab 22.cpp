5170c02036af316631fa88ff0c8bc0a0f26d47fe
Dr. Alsayed <Dr. Alsayed@DESKTOP-APIOASQ>
2025-07-29 21:23:37 +0300

    Add project files.

---
+++ Lab 22/Lab 22.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int num=0, sum = 0;
+
+	for (num=0;num < 10;num++) {
+		
+		sum = sum + num;
+		
+	}
+	
+	cout << sum;
+	
+	
+	
+	
+
+	//while (num < 10) {
+
+	//sum = sum + num;
+
+	//num++;
+	//}
+
+//	cout << sum;
+
+	
+	
+	
+	
+	return 0;
+}
+
